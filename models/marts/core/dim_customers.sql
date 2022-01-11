3f2f5b4768fab5b7a785292e8c2769affbaceff6
Michel Hua <michel.hua+training@artefact.com>
2022-01-11 20:24:48 +0000

    solution

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,13 +1,9 @@
 with customers as (
-    select * from {{ ref('stg_customers') }}
+    select * from {{ ref('stg_customers')}}
 ),
-
 orders as (
-    select * from {{ ref('stg_orders') }}
-    join {{ ref('fct_orders') }}
-    on {{ ref('stg_orders') }}.order_id = {{ ref('fct_orders') }}.order_id
+    select * from {{ ref('fct_orders')}}
 ),
-
 customer_orders as (
     select
         customer_id,
@@ -18,7 +14,6 @@ customer_orders as (
     from orders
     group by 1
 ),
-
 final as (
     select
         customers.customer_id,
@@ -27,9 +22,9 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customers.lifetime_value as lifetime_value
+        customer_orders.lifetime_value
     from customers
     left join customer_orders using (customer_id)
 )
-
 select * from final
+
--- models/staging/jaffle_shop/stg_payment.sql
+++
@@ -1,4 +0,0 @@
-select 
-    orderid as order_id, amount
-
-from raw.stripe.payment
