846bfc1f060860e4c613fcf679ecf03859631bf5
Michel Hua <michel.hua+training@artefact.com>
2022-01-23 20:21:51 +0000

    window function

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,4 +1,25 @@
-with 
+with orders as (
+    select * from {{ source('jaffle_shop', 'orders') }}
+),
+
+payments as (
+    select * from {{ source('stripe', 'payment') }}
+),
+
+customers as (
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
+
+completed_payments as (
+    select 
+        orderid as order_id, 
+        max(created) as payment_finalized_date, 
+        sum(amount) / 100.0 as total_amount_paid
+    from payments
+    where status <> 'fail'
+    group by 1
+),
+
 paid_orders as (
     select orders.id as order_id,
         orders.user_id	as customer_id,
@@ -6,50 +27,44 @@ paid_orders as (
         orders.status as order_status,
         p.total_amount_paid,
         p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
-    from raw.jaffle_shop.orders as orders
-    left join (
-        select 
-            orderid as order_id, 
-            max(created) as payment_finalized_date, 
-            sum(amount) / 100.0 as total_amount_paid
-        from raw.stripe.payment
-        where status <> 'fail'
-        group by 1
-    ) p on orders.id = p.order_id
-    left join raw.jaffle_shop.customers c on orders.user_id = c.id ),
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
+    from orders
+    left join completed_payments p on orders.id = p.order_id
+    left join customers on orders.user_id = customers.id 
+),
 
 customer_orders as (
     select 
-        c.id as customer_id, 
+        customers.id as customer_id, 
         min(order_date) as first_order_date, 
         max(order_date) as most_recent_order_date, 
         count(orders.id) as number_of_orders
-    from raw.jaffle_shop.customers c
-    left join raw.jaffle_shop.orders as orders on orders.user_id = c.id
+    from customers
+    left join raw.jaffle_shop.orders as orders on orders.user_id = customers.id
     group by 1
+),
+
+final as (
+
+    select
+        paid_orders.*,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
+        -- new vs returning customer
+        case when customer_orders.first_order_date = paid_orders.order_placed_at
+        then 'new'
+        else 'return' end as nvsr,
+        -- customer lifetime value
+        sum(total_amount_paid) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+        ) as customer_lifetime_value,
+        -- first day of sale
+        customer_orders.first_order_date as fdos
+    from paid_orders
+    left join customer_orders using (customer_id)
+    order by order_id
 )
 
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case when c.first_order_date = p.order_placed_at
-    then 'new'
-    else 'return' end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join
-(
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
-order by order_id
+select * from final
